2ae352e778d33e922c40c0799e94405a92dae511
azatAkash <azatakash14@gmail.com>
2025-09-20 08:38:17 +0500

    refactor: move orders page from starting code to react

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,11 +24,11 @@ const HomePage = () => {
         </div>
 
         <div className="right-section">
-          <a className="orders-link header-link" href="orders.html">
+          <a className="orders-link header-link" href="/orders">
             <span className="orders-text">Orders</span>
           </a>
 
-          <a className="cart-link header-link" href="checkout">
+          <a className="cart-link header-link" href="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
